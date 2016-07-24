4019b2882457fcb8713f268556f2c85f8aed0fc5
tokestermw <tokestermw@gmail.com>
2016-07-24 01:48:43 -0700

    added ability to use SCRNNs

--- rnn.py
+++ rnn.py
@@ -1,5 +1,8 @@
 from __future__ import division
 
+import plac
+from tqdm import tqdm
+
 import numpy as np
 import tensorflow as tf
 
@@ -10,8 +13,9 @@ RNN_HIDDEN_SIZE = 32
 
 
 class RNNModel(object):
-    def __init__(self, vocab_size, 
-        n_steps=N_STEPS, embeddings_size=EMBEDDINGS_SIZE, rnn_hidden_size=RNN_HIDDEN_SIZE):
+    def __init__(self, vocab_size, rnn_type="rnn",
+                 n_steps=N_STEPS, embeddings_size=EMBEDDINGS_SIZE, rnn_hidden_size=RNN_HIDDEN_SIZE):
+        """ RNN Model object """
 
         """ The char or word IDs for input and target. """
         self.input_ids = tf.placeholder(tf.int32, [None, n_steps])
@@ -27,7 +31,17 @@ class RNNModel(object):
 
         """ set up RNN cells """
         with tf.variable_scope("rnn_model"):
-            self.cell = tf.nn.rnn_cell.BasicRNNCell(rnn_hidden_size)
+            from fast_and_slow import TraceRNNCell, SCRNNCell
+
+            if rnn_type == "rnn":
+                self.cell = tf.nn.rnn_cell.BasicRNNCell(rnn_hidden_size)
+            elif rnn_type == "lstm":
+                self.cell = tf.nn.rnn_cell.BasicLSTMCell(rnn_hidden_size, state_is_tuple=True)
+            elif rnn_type == "trace":
+                self.cell = TraceRNNCell(rnn_hidden_size)
+            elif rnn_type == "scrnn":
+                self.cell = SCRNNCell(rnn_hidden_size, state_is_tuple=True)
+
             self.softmax_w = tf.get_variable("softmax_w", [rnn_hidden_size, vocab_size])
             self.softmax_b = tf.get_variable("softmax_b", [vocab_size])
 
@@ -36,8 +50,6 @@ class RNNModel(object):
                 time_major=False, dtype=tf.float32)
 
         """ set up loss """
-        # [n_steps, batch_size, rnn_hidden_size]
-        # outputs = tf.transpose(self._outputs, [1, 0, 2])
         # [n_steps * batch_size, rnn_hidden_size]
         self.outputs = tf.reshape(self._outputs, [-1, rnn_hidden_size])
         # [n_steps * batch_size, vocab_size]
@@ -52,18 +64,10 @@ class RNNModel(object):
         # [1]
         self.cost = tf.reduce_sum(self.loss) / tf.to_float(batch_size) / tf.to_float(n_steps)
 
-        self.lr = 0.03 # tf.Variable(0.0, trainable=False)
-        # tvars = tf.trainable_variables()
-        # grads, _ = tf.clip_by_global_norm(tf.gradients(self.cost, tvars),
-                # 5.0)
-        # grads = tf.gradients(self.cost, tvars)
-        # self.optimizer = tf.train.AdamOptimizer(self.lr)
-        # self.train_op = self.optimizer.apply_gradients(zip(grads, tvars))
-        # grads_and_vars = self.optimizer.compute_gradients(self.cost)
-        # self.train_op = self.optimizer.apply_gradients(grads_and_vars)
-        # print "kdfjkdfjdkfj", type(grads)
-
+        self.lr = 0.01 # tf.Variable(0.0, trainable=False)
+        
         self.optimizer = tf.train.AdamOptimizer(self.lr)
+        # self.optimizer = tf.train.GradientDescentOptimizer(self.lr)
         self.train_op = self.optimizer.minimize(self.cost)
 
     def step(self, session, input_ids, target_ids, verbose=False):
@@ -71,51 +75,43 @@ class RNNModel(object):
             self.input_ids: input_ids,
             self.target_ids: target_ids,
         }
-        if verbose:
-            cost, loss, probs, inputs, _outputs, outputs, targets, logits, softmax_w, softmax_b = session.run([self.cost, self.loss, self.probs, self.inputs, self._outputs, self.outputs, self.targets, self.logits, self.softmax_w, self.softmax_b], feed_dict=feed_dict)
-            print 'cost', cost
-            print 'loss', loss.shape
-            print 'probs', probs.shape
-            print 'inputs', inputs.shape
-            print '_outputs', _outputs.shape
-            print 'outputs', outputs.shape
-            print 'targets', targets.shape
-            print 'logits', logits.shape        
-            print 'softmax_w', softmax_w.shape
-            print 'softmax_b', softmax_b.shape
-        else:
-            cost, _ = session.run([self.cost, self.train_op], feed_dict=feed_dict)
+        cost, _ = session.run([self.cost, self.train_op], feed_dict=feed_dict)
         return cost
 
+    def decode(self, session, inputs, temperature=1.0):
+        pass
 
-def test():
+
+@plac.annotations(
+    rnn_type=("RNN type."),
+)
+def test(rnn_type="rnn"):
     from data import loop_data, build_vocabulary, batchify
 
-    batch_size = 16
-    n_steps = 50
+    batch_size = 32
+    n_steps = 20
 
     text = loop_data()
     vocab, rev_vocab = build_vocabulary(text)
     vocab_size = len(vocab)
     print vocab_size
 
-    model = RNNModel(vocab_size, n_steps=n_steps)
+    model = RNNModel(vocab_size, n_steps=n_steps, rnn_type=rnn_type)
 
+    # TODO: sample decoded sentence
     with tf.Session() as sess:
         tf.initialize_all_variables().run()
-        for epoch in range(10):
+        for epoch in range(5):
             print "epoch", epoch
             list_of_costs = []
-            for idx, (x, y) in enumerate(batchify(text, vocab, batch_size, n_steps)):
+            for idx, (x, y) in tqdm(enumerate(batchify(text, vocab, batch_size, n_steps))):
                 list_of_costs.append(model.step(sess, x, y))
                 if idx % 100 == 0:
                     print "cost", np.mean(list_of_costs)
-                    # print "x", ''.join([rev_vocab[i] for i in x[0]])
-                    # print "y", ''.join([rev_vocab[i] for i in y[0]])
                     list_of_costs = []
 
 
 if __name__ == '__main__':
-    test()
+    plac.call(test)
 
 
