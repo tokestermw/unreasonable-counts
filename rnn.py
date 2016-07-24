d11e8040f095b9f8f79dedd8d3b5e421df600fc0
tokestermw <tokestermw@gmail.com>
2016-07-24 15:05:44 -0700

    1) assign learning rate 2) learning rate decay 3) validation error

--- rnn.py
+++ rnn.py
@@ -13,7 +13,7 @@ RNN_HIDDEN_SIZE = 32
 
 
 class RNNModel(object):
-    def __init__(self, vocab_size, rnn_type="rnn",
+    def __init__(self, vocab_size, rnn_type="rnn", 
                  n_steps=N_STEPS, embeddings_size=EMBEDDINGS_SIZE, rnn_hidden_size=RNN_HIDDEN_SIZE):
         """ RNN Model object """
 
@@ -41,12 +41,14 @@ class RNNModel(object):
                 self.cell = TraceRNNCell(rnn_hidden_size)
             elif rnn_type == "scrnn":
                 self.cell = SCRNNCell(rnn_hidden_size, state_is_tuple=True)
+            else:
+                raise Exception("Choose correct rnn_type.")
 
             self.softmax_w = tf.get_variable("softmax_w", [rnn_hidden_size, vocab_size])
             self.softmax_b = tf.get_variable("softmax_b", [vocab_size])
 
             # [batch_size, n_steps, rnn_hidden_size]
-            self._outputs, _ = tf.nn.dynamic_rnn(self.cell, self.inputs,
+            self._outputs, _final_state = tf.nn.dynamic_rnn(self.cell, self.inputs,
                 time_major=False, dtype=tf.float32)
 
         """ set up loss """
@@ -64,54 +66,83 @@ class RNNModel(object):
         # [1]
         self.cost = tf.reduce_sum(self.loss) / tf.to_float(batch_size) / tf.to_float(n_steps)
 
-        self.lr = 0.01 # tf.Variable(0.0, trainable=False)
+        self.lr = tf.Variable(0.0, trainable=False)
+        # self.lr = 0.01
         
         self.optimizer = tf.train.AdamOptimizer(self.lr)
-        # self.optimizer = tf.train.GradientDescentOptimizer(self.lr)
         self.train_op = self.optimizer.minimize(self.cost)
 
-    def step(self, session, input_ids, target_ids, verbose=False):
+    def step(self, session, input_ids, target_ids, is_train=True, verbose=False):
         feed_dict = {
             self.input_ids: input_ids,
             self.target_ids: target_ids,
         }
-        cost, _ = session.run([self.cost, self.train_op], feed_dict=feed_dict)
+        if is_train:
+            cost, _ = session.run([self.cost, self.train_op], feed_dict=feed_dict)
+        else:
+            cost, logits = session.run([self.cost, self.logits], feed_dict=feed_dict)
         return cost
 
     def decode(self, session, inputs, temperature=1.0):
         pass
 
+    def assign_lr(self, session, lr_value):
+        session.run(tf.assign(self.lr, lr_value))
+
 
 @plac.annotations(
     rnn_type=("RNN type."),
 )
-def test(rnn_type="rnn"):
+def main(rnn_type="rnn"):
     from data import loop_data, build_vocabulary, batchify
+
+    np.random.seed(11)
 
     batch_size = 32
     n_steps = 20
+    lr = 0.01
+    lr_decay = 0.5
 
-    text = loop_data()
-    vocab, rev_vocab = build_vocabulary(text)
+    train_text, valid_text = loop_data()
+
+    vocab, rev_vocab = build_vocabulary(train_text)
     vocab_size = len(vocab)
-    print vocab_size
+    print "vocab size:", vocab_size
 
     model = RNNModel(vocab_size, n_steps=n_steps, rnn_type=rnn_type)
 
     # TODO: sample decoded sentence
     with tf.Session() as sess:
         tf.initialize_all_variables().run()
+
+        prev_epoch_cost = 9999999  # arbitarily large number
         for epoch in range(5):
             print "epoch", epoch
+            print "learning rate", lr
+
             list_of_costs = []
-            for idx, (x, y) in tqdm(enumerate(batchify(text, vocab, batch_size, n_steps))):
-                list_of_costs.append(model.step(sess, x, y))
+            model.assign_lr(sess, lr)
+
+            for idx, (x, y) in tqdm(enumerate(batchify(train_text, vocab, batch_size, n_steps))):
+                list_of_costs.append(model.step(sess, x, y, is_train=True))
                 if idx % 100 == 0:
-                    print "cost", np.mean(list_of_costs)
+                    print "cost", 2 ** np.mean(list_of_costs)
                     list_of_costs = []
+
+            epoch_cost = np.mean(list_of_costs)
+            print "train cost", 2 ** epoch_cost
+
+            list_of_costs = []
+            for idx, (x, y) in tqdm(enumerate(batchify(valid_text, vocab, batch_size, n_steps))):
+                list_of_costs.append(model.step(sess, x, y, is_train=False))
+
+            epoch_cost = np.mean(list_of_costs)
+            print "valid cost", 2 ** epoch_cost
+
+            if epoch_cost > prev_epoch_cost:
+                lr *= lr_decay
+            prev_epoch_cost = epoch_cost
 
 
 if __name__ == '__main__':
-    plac.call(test)
-
-
+    plac.call(main)
