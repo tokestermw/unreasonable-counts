cc46bff8181122a4ba3f2857919b846a2ed5b6cf
tokestermw <tokestermw@gmail.com>
2016-07-24 01:47:52 -0700

    update to tensorflow 0.9.0 and added tqdm

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+echo "install python dependencies"
 easy_install --upgrade six
-pip install --upgrade https://storage.googleapis.com/tensorflow/mac/tensorflow-0.8.0-py2-none-any.whl
+pip install --upgrade https://storage.googleapis.com/tensorflow/mac/tensorflow-0.9.0-py2-none-any.whl
 pip install -r requirements.txt
